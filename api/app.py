1c60728289580f1f39f7370202c905fac3b26ed3
Nero <nerocube.tw@gmail.com>
2018-12-31 19:03:20 +0800

    pic to numpy array

--- api/app.py
+++ api/app.py
@@ -1,7 +1,7 @@
 import numpy as np
 import pandas as pd
 from flask import Flask, jsonify, request
-import gunicorn
+import gunicorn, cv2, io
 
 app = Flask(__name__)
 
@@ -9,3 +9,13 @@ app = Flask(__name__)
 @app.route("/")
 def hello():
     return "Welcome to ML Service!"
+
+@app.route('/predict', methods = ['POST'])
+def upload_file():
+    photo = request.files['file']
+    in_memory_file = io.BytesIO()
+    photo.save(in_memory_file)
+    data = np.fromstring(in_memory_file.getvalue(), dtype=np.uint8)
+    color_image_flag = 1
+    img = cv2.imdecode(data, color_image_flag)
+    return '{}'.format(img)
